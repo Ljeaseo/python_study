36aa2c5e2615540e554a9c33899e4e6d740f6005
Ljeaseo <ljseo0111@naver.com>
2022-08-24 21:51:54 +0900

    py0824

--- homework04.py
+++ homework04.py
@@ -66,6 +66,9 @@ if __name__=="__main__":
 #     sum+=list[i]
 #     avg = sum/list[i]
 # print("총점 :",sum,"평균 :",avg.f2)
+'''
+from random import random
+
 
 if __name__ == "__main__":
     
@@ -83,3 +86,79 @@ if __name__ == "__main__":
     avg = total / len(myList)
 
     print(f'총점 {total}, 평균 {avg:.2f}')
+'''
+
+'''
+import random
+import os
+
+trys = 0
+answers = 0
+
+while True:
+
+    # 값 생성
+    num1 = random.randint(0,10)
+    num2 = random.randint(0,10)
+
+    print(f"{num1} + {num2} = ")
+    result = int(input("덧셈의 값은?"))
+    
+    if(result == num1+num2):   #정답일때
+        print("정답입니다.")
+        answers += 1
+        trys += 1
+    else:                      #오답일때
+        print("오답입니다.")    
+        trys += 1
+    
+    #문제를 더 풀지 말지 선택후 터미널 clear 
+    stop = int(input("그만하고 싶으면 0, 더하고 싶으면 1"))
+    os.system('cls')  
+    
+    #문제를 풀지 않겟다고 선택했을 때
+    if(stop == 0):
+        print(f"시도횟수 : {trys}, 맞은 횟수: {answers}")
+        break
+''' 
+
+
+#물건값 입력
+price = int(input("물건의 값을 입력하세요:"))
+#지불한 금액
+pay = int(input("지불할 금액을 입력하세요:"))
+
+#거스름돈
+change = pay-price
+print(change)
+
+money1000 = change//1000
+change = change%1000
+
+money500 = change//500
+change = change%500
+
+money100 = change//100
+change = change%100
+
+money10 = change//10
+change = change%10 
+
+print(f"거스름돈은:{change}, 천원짜리는{money1000}장, 오백원짜리는{money500}개,백원짜리는{money100}개,십원짜리는{money10}개")
+
+
+
+
+
+
+
+
+    
+        
+    
+ 
+    
+    
+
+    
+    
